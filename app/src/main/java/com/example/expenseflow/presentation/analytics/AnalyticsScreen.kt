b9f5d2af56ea270975aeb5f9f7a268dae08149fe
Vignesh Subramaniam <messivignesh07@gmail.com>
2025-12-04 09:15:22 -0500

    Analytics updated one

--- app/src/main/java/com/example/expenseflow/presentation/analytics/AnalyticsScreen.kt
+++ app/src/main/java/com/example/expenseflow/presentation/analytics/AnalyticsScreen.kt
@@ -1,8 +1,16 @@
 package com.example.expenseflow.presentation.analytics
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,22 +19,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import co.yml.charts.axis.AxisData
 import co.yml.charts.common.model.Point
@@ -34,41 +58,273 @@ import co.yml.charts.ui.barchart.BarChart
 import co.yml.charts.ui.barchart.models.BarChartData
 import co.yml.charts.ui.barchart.models.BarChartType
 import co.yml.charts.ui.barchart.models.BarData
+import com.example.expenseflow.R
 import com.example.expenseflow.viewmodel.AnalyticsUiState
 import com.example.expenseflow.viewmodel.AnalyticsViewModel
 
 @Composable
-fun AnalyticsScreen(modifier: Modifier = Modifier) {
-    CustomBarChart()
+fun AnalyticsScreen(modifier: Modifier = Modifier, viewModel: AnalyticsViewModel = viewModel()) {
+    Column {
+        OverallScreenModules(viewModel = viewModel)
+
+    }
+}
+
+
+@Composable
+fun OverallScreenModules(modifier: Modifier = Modifier, viewModel: AnalyticsViewModel) {
+
+    val selectedRange by viewModel.selectedRange.collectAsState()
+    Column {
+        HeadingSection()
+        Spacer(modifier.height(20.dp))
+        TriToggle(selectedIndex = selectedRange, onSelected = { viewModel.updateSelectedRange(it) })
+        Spacer(modifier.height(20.dp))
+        TriCards(viewModel = viewModel)
+        Spacer(modifier.height(20.dp))
+        BarchartSection()
+    }
 }
 
 @Composable
-fun CustomBarChart(modifier: Modifier = Modifier, viewModel: AnalyticsViewModel = viewModel()) {
-    val data = listOf(100f, 80f, 150f, 180f)
-    val labels = listOf("June", "Jul", "Aug", "Sep")
+fun TriCards(modifier: Modifier = Modifier, viewModel: AnalyticsViewModel) {
 
-    Canvas(modifier = Modifier.size(300.dp)) {
-        val barWidth = 40f
-        val spaceBetween = 40f
-        var currentX = 30f
+    val total by viewModel.activeTotal.collectAsState(initial = 0)
+    val average by viewModel.activeAverage.collectAsState(initial = 0.0)
+    val count by viewModel.activeCount.collectAsState(initial = 0)
 
-        data.forEachIndexed { idx, d ->
-            val barHeight = d
-            drawRect(
-                color = Color.Green,
-                topLeft = Offset(currentX, size.height - 50f - barHeight),
-                size = Size(barWidth, barHeight)
-            )
-            drawContext.canvas.nativeCanvas.drawText(
-                labels[idx],
-                currentX,
-                size.height - 20f, // bottom
-                android.graphics.Paint().apply {
-                    color = android.graphics.Color.BLACK
-                    textSize = 24f
+
+    Row(
+        modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ElevatedCard(modifier.size(120.dp)) {
+            Column(
+                modifier
+                    .fillMaxSize()
+                    .padding(top = 5.dp, bottom = 5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.grossprofit),
+                    contentDescription = null,
+                    modifier.size(50.dp)
+                )
+                Spacer(modifier.height(8.dp))
+                Text("Total")
+                Spacer(modifier.height(10.dp))
+                Text("$$total", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
+
+            }
+        }
+        ElevatedCard(modifier.size(120.dp)) {
+            Column(
+                modifier
+                    .fillMaxSize()
+                    .padding(top = 5.dp, bottom = 5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.grossprofit),
+                    contentDescription = null,
+                    modifier.size(50.dp)
+                )
+                Spacer(modifier.height(8.dp))
+                Text("Daily Avg")
+                Spacer(modifier.height(10.dp))
+                Text("$$average", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
+
+            }
+        }
+        ElevatedCard(modifier.size(120.dp)) {
+            Column(
+                modifier
+                    .fillMaxSize()
+                    .padding(top = 5.dp, bottom = 5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.grossprofit),
+                    contentDescription = null,
+                    modifier.size(50.dp)
+                )
+                Spacer(modifier.height(8.dp))
+                Text("Expenses")
+                Spacer(modifier.height(10.dp))
+                Text("$count", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun TriToggle(
+    selectedIndex: Int,
+    onSelected: (Int) -> Unit
+) {
+    val headers = listOf("This Month", "3 Months", "This year")
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp)
+            .height(50.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            headers.forEachIndexed { idx, header ->
+                val isSelected = idx == selectedIndex
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = if (isSelected) Color.White.copy(
+                            alpha = 0.6f
+                        ) else Color.Transparent,
+                        contentColor = if (isSelected) Color.Black else Color.Gray
+                    ), onClick = { onSelected(idx) }
+                ) {
+                    Text(
+                        header,
+                        fontSize = 16.sp,
+                        fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+                        modifier = Modifier
+                            .padding(horizontal = 12.dp, vertical = 12.dp)
+                    )
                 }
-            )
-            currentX += barWidth + spaceBetween
+            }
+        }
+    }
+}
+
+@Composable
+fun HeadingSection(modifier: Modifier = Modifier) {
+    Column(
+        modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Analytics", fontSize = 24.sp, fontWeight = FontWeight.ExtraBold)
+        Spacer(modifier.height(20.dp))
+        Text(
+            "Understand your spending patterns",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Gray
+        )
+    }
+}
+
+@Composable
+fun BarchartSection(modifier: Modifier = Modifier) {
+    ElevatedCard(
+        modifier
+            .fillMaxWidth()
+            .padding(horizontal = 30.dp)
+            .height(200.dp)
+    ) {
+        CustomBarChart()
+    }
+}
+
+
+@Composable
+fun CustomBarChart(modifier: Modifier = Modifier, viewModel: AnalyticsViewModel = viewModel()) {
+
+    val data by viewModel.chartPoints.collectAsState()
+    val labels by viewModel.xLabel.collectAsState()
+
+    Column(
+        modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement =
+            Arrangement.Center
+    ) {
+        var startAnimation by remember { mutableStateOf(false) }
+
+        LaunchedEffect(true) {
+            startAnimation = true
+        }
+        val animatedHeight = data.mapIndexed { idx, value ->
+            animateFloatAsState(
+                targetValue = if (startAnimation) value else 0f,
+                animationSpec = tween(
+                    durationMillis = 2000,
+                    delayMillis = idx * 120,
+                    easing = FastOutSlowInEasing
+                )
+            ).value
+        }
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val barCount = data.size
+
+            val bottomPadding = 60f
+            val totalWidth = size.width
+            val spaceRatio = 0.2f
+            val segmentWidth = totalWidth / barCount
+            val barWidth = segmentWidth * (1f - spaceRatio)
+            val spaceBetween = segmentWidth * spaceRatio
+
+            var currentX = 0f
+
+            data.forEachIndexed { idx, value ->
+
+                val height = animatedHeight[idx]
+                val barX = currentX + (segmentWidth - barWidth) / 2f
+                val radius = 20f
+
+// Create a round-rect but only round the top corners
+                val barRect = androidx.compose.ui.geometry.Rect(
+                    barX,
+                    size.height - bottomPadding - height,
+                    barX + barWidth,
+                    size.height - bottomPadding
+                )
+
+                val roundRect = RoundRect(
+                    rect = barRect,
+                    topLeft = CornerRadius(radius, radius),
+                    topRight = CornerRadius(radius, radius),
+                    bottomLeft = CornerRadius(0f, 0f),
+                    bottomRight = CornerRadius(0f, 0f)
+                )
+
+                val path = Path().apply {
+                    addRoundRect(roundRect)
+                }
+
+// Clip to the path and draw inside it
+                clipPath(path) {
+                    drawRect(
+                        color = Color(0xFF4CAF50),
+                        topLeft = Offset(barRect.left, barRect.top),
+                        size = Size(barWidth, height)
+                    )
+                }
+
+                drawContext.canvas.nativeCanvas.apply {
+                    val paint = android.graphics.Paint().apply {
+                        color = android.graphics.Color.BLACK
+                        textSize = 30f
+                        textAlign = android.graphics.Paint.Align.CENTER
+                    }
+
+                    drawText(
+                        labels[idx],
+                        barX + barWidth / 2f,   // center of bar
+                        size.height - 20f,
+                        paint
+                    )
+                }
+                currentX += segmentWidth
+            }
         }
     }
 }
--- app/src/main/java/com/example/expenseflow/viewmodel/AnalyticsViewModel.kt
+++ app/src/main/java/com/example/expenseflow/viewmodel/AnalyticsViewModel.kt
@@ -6,60 +6,100 @@ import co.yml.charts.common.model.Point
 import com.example.expenseflow.data.model.Expense
 import com.example.expenseflow.data.repository.TransactionsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 import java.util.Locale
+import java.util.function.DoubleUnaryOperator
+import kotlin.math.exp
+import kotlin.math.sign
 
 
-sealed class AnalyticsUiState(){
+sealed class AnalyticsUiState() {
     object Idle : AnalyticsUiState()
     object Loading : AnalyticsUiState()
-    data class Success(val expense : List<Expense>) : AnalyticsUiState()
-    data class Error(val message : String) : AnalyticsUiState()
+    data class Success(val expense: List<Expense>) : AnalyticsUiState()
+    data class Error(val message: String) : AnalyticsUiState()
 }
 
 class AnalyticsViewModel : ViewModel() {
     private val repository = TransactionsRepository()
 
     private val _uiState = MutableStateFlow<AnalyticsUiState>(AnalyticsUiState.Idle)
-    val uiState : StateFlow<AnalyticsUiState> = _uiState
+    val uiState: StateFlow<AnalyticsUiState> = _uiState
 
-    private val _chartPoints = MutableStateFlow<List<Point>>(emptyList())
-    val chartPoints : StateFlow<List<Point>> = _chartPoints
+    private val _chartPoints = MutableStateFlow<List<Float>>(emptyList())
+    val chartPoints: StateFlow<List<Float>> = _chartPoints
 
     private val _xLabel = MutableStateFlow<List<String>>(emptyList())
-    val xLabel : StateFlow<List<String>> = _xLabel
+    val xLabel: StateFlow<List<String>> = _xLabel
+// Expense Average ----------------------------------------------------------------------
+    private val _monthlyExpense = MutableStateFlow(0.00)
+    val monthlyExpense: StateFlow<Double> = _monthlyExpense
 
+    private val _monthlyDailyAverage = MutableStateFlow(0.0)
+    val monthlyDailyAverage: StateFlow<Double> = _monthlyDailyAverage
+
+    private val _thisMonthExpense = MutableStateFlow(0)
+    val thisMonthExpense: StateFlow<Int> = _thisMonthExpense
+
+    private val _threeMonthExpense = MutableStateFlow(0)
+    val threeMonthExpense: StateFlow<Int> = _threeMonthExpense
+
+    private val _threeMonthAverage = MutableStateFlow(0.0)
+    val threeMonthAverage: StateFlow<Double> = _threeMonthAverage
+
+    private val _threeMonthExpenseTotal = MutableStateFlow(0.0)
+    val threeMonthsExpenseTotal: StateFlow<Double> = _threeMonthExpenseTotal
+
+    private val _yearlyExpense = MutableStateFlow(0)
+    val yearlyExpense: StateFlow<Int> = _yearlyExpense
+
+    private val _yearlyAverage = MutableStateFlow(0.0)
+    val yearlyAverage: StateFlow<Double> = _yearlyAverage
+
+    private val _yearlyExpenseTotal = MutableStateFlow(0.0)
+    val yearlyExpenseTotal: StateFlow<Double> = _yearlyExpenseTotal
+
+
+    private val _selectedRange = MutableStateFlow(0)
+    val selectedRange : StateFlow<Int> = _selectedRange
+
+    //----------------------------------------------------------------------//
     private val dateFormatterPattern = DateTimeFormatter.ofPattern("MMM d ,yyyy", Locale.ENGLISH)
 
 
     init {
-
+        loadExpenses()
     }
 
-    fun loadExpenses(){
+    fun loadExpenses() {
         viewModelScope.launch {
             _uiState.value = AnalyticsUiState.Loading
             try {
                 val expense = repository.getExpenses()
                 _uiState.value = AnalyticsUiState.Success(expense)
                 chartData(expense)
-            }catch (e : Exception){
-                _uiState.value = AnalyticsUiState.Error(e.localizedMessage ?: "Something went wrong on our side")
+                expenseCalculator(expense)
+            } catch (e: Exception) {
+                _uiState.value =
+                    AnalyticsUiState.Error(e.localizedMessage ?: "Something went wrong on our side")
             }
         }
     }
 
-    private fun chartData(expense: List<Expense>){
+    private fun chartData(expense: List<Expense>) {
         if (expense.isEmpty()) return
 
         val endMonth = YearMonth.now()
         val startMonth = endMonth.minusMonths(5)
 
-        val months = (0L .. 5L).map { startMonth.plusMonths(it) }
+        val months = (0L..5L).map { startMonth.plusMonths(it) }
 
         val byMonthTotals: Map<YearMonth, Double> = expense
             .mapNotNull { e ->
@@ -74,10 +114,10 @@ class AnalyticsViewModel : ViewModel() {
             .groupBy({ it.first }, { it.second })
             .mapValues { (_, amounts) -> amounts.sum() }
 
-        val orderTotals = months.map { ym -> byMonthTotals[ym] ?:0.0 }
+        val orderTotals = months.map { ym -> byMonthTotals[ym] ?: 0.0 }
 
-        val points = orderTotals.mapIndexed { idx,total ->
-            Point(x=idx.toFloat(),y=total.toFloat())
+        val points = orderTotals.map { total ->
+            total.toFloat()
         }
 
         val labelFmt = DateTimeFormatter.ofPattern("MMM", Locale.ENGLISH)
@@ -87,4 +127,113 @@ class AnalyticsViewModel : ViewModel() {
         _chartPoints.value = points
         _xLabel.value = labels
     }
+
+    private fun expenseCalculator(expense: List<Expense>) {
+
+        val currentLocalDate = LocalDate.now()
+        val currentMonth = currentLocalDate.month
+        val currentYear = currentLocalDate.year
+        val threeMonths = currentMonth.minus(3)
+        val oneYear = currentMonth.minus(12)
+
+        val filteredExpenseByMonth = expense.mapNotNull { expense ->
+            try {
+                val date = LocalDate.parse(expense.date, dateFormatterPattern)
+                if (date.month == currentMonth && date.year == currentYear) expense.amount
+                else null
+            } catch (_: Exception) {
+                null
+            }
+        }.sum()
+
+
+        val expenseCount = expense.count { expense ->
+            try {
+                val date = LocalDate.parse(expense.date, dateFormatterPattern)
+                date.month == currentMonth && date.year == currentYear
+            } catch (_: Exception) {
+                false
+            }
+        }
+        _monthlyExpense.value = filteredExpenseByMonth
+        _thisMonthExpense.value = expenseCount
+        _monthlyDailyAverage.value = filteredExpenseByMonth / 30
+
+
+        val filteredExpenseBy3Month = expense.mapNotNull { expense ->
+            try {
+                val date = LocalDate.parse(expense.date, dateFormatterPattern)
+                if (date.month == threeMonths && date.year == currentYear) expense.amount
+                else null
+            } catch (_: Exception) {
+                null
+            }
+        }.sum()
+
+        val threeMonthExpense = expense.count { expense ->
+            try {
+                val date = LocalDate.parse(expense.date, dateFormatterPattern)
+                date.month == threeMonths && date.year == currentYear
+            } catch (_: Exception) {
+                false
+            }
+        }
+
+        _threeMonthExpenseTotal.value = filteredExpenseBy3Month
+        _threeMonthExpense.value = threeMonthExpense
+        _threeMonthAverage.value = filteredExpenseBy3Month / 92
+
+        val filteredExpenseByYear = expense.mapNotNull { expense ->
+            try {
+                val date = LocalDate.parse(expense.date, dateFormatterPattern)
+                if (date.month == oneYear) expense.amount
+                else null
+            } catch (_: Exception) {
+                null
+            }
+        }.sum()
+
+        val oneYearExpense = expense.count{expense ->
+            try {
+                val date = LocalDate.parse(expense.date, dateFormatterPattern)
+                date.month == oneYear
+            } catch (_: Exception) {
+                false
+            }
+        }
+
+        _yearlyExpense.value = oneYearExpense
+        _yearlyExpenseTotal.value = filteredExpenseByYear
+        _yearlyAverage.value = filteredExpenseByYear/365
+    }
+
+    fun updateSelectedRange(index : Int){
+        _selectedRange.value = index
+    }
+
+    val activeTotal : StateFlow<Double> = selectedRange.map { range ->
+        when(range){
+            0 -> monthlyExpense.value
+            1 -> threeMonthsExpenseTotal.value
+            2 -> yearlyExpenseTotal.value
+            else -> 0.0
+        }
+    }.stateIn(viewModelScope , SharingStarted.Eagerly,0.0)
+
+    val activeAverage : StateFlow<Double> = selectedRange.map { range->
+        when(range){
+            0 -> monthlyDailyAverage.value
+            1 -> threeMonthAverage.value
+            2 -> yearlyAverage.value
+            else -> 0.0
+        }
+    }.stateIn(viewModelScope , SharingStarted.Eagerly , 0.0)
+    val activeCount : StateFlow<Int> = selectedRange.map { range ->
+        when(range){
+            0 -> thisMonthExpense.value
+            1 -> threeMonthExpense.value
+            2 ->yearlyExpense.value
+            else -> 0
+        }
+    } . stateIn(viewModelScope , SharingStarted.Eagerly , 0)
 }
